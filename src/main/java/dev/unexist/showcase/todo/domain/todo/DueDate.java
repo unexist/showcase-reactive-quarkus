84861726e45571be8ed5ecef448c1e10c4a48dff
Christoph Kappel <christoph@unexist.dev>
2021-08-11 14:50:11 +0200

    Added date serializer

--- src/main/java/dev/unexist/showcase/todo/domain/todo/DueDate.java
+++ src/main/java/dev/unexist/showcase/todo/domain/todo/DueDate.java
@@ -11,23 +11,53 @@
 
 package dev.unexist.showcase.todo.domain.todo;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import dev.unexist.showcase.todo.infrastructure.serializer.DateSerializer;
+
 import java.time.LocalDate;
 
 public class DueDate {
+    @JsonSerialize(using = DateSerializer.class)
     private LocalDate start;
+
+    @JsonSerialize(using = DateSerializer.class)
     private LocalDate due;
+
+    /**
+     * Get start date
+     *
+     * @return Start date
+     **/
 
     public LocalDate getStart() {
         return start;
     }
 
+    /**
+     * Set start date
+     *
+     * @param  start  Date to set
+     **/
+
     public void setStart(LocalDate start) {
         this.start = start;
     }
 
+    /**
+     * Get due date
+     *
+     * @return Due date
+     **/
+
     public LocalDate getDue() {
         return due;
     }
+
+    /**
+     * Set due date
+     *
+     * @param  due  Date to set
+     **/
 
     public void setDue(LocalDate due) {
         this.due = due;
